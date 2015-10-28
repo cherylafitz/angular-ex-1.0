1d4e5b609a7c00b0590b71ab2fa579679b85b1ff
cherylafitz <cherylafitzgerald@gmail.com>
2015-10-28 15:01:54 -0700

    fixed for deployment

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ var methodOverride = require('method-override');
 
 // configuration =================
 
-// Connect to the db
-
-// var db = mongoose.connection;
-
-// db.on('error', console.error);
-// db.once('open', function() {
-//   // Create your schemas and models here.
-// });
-
-// mongoose.connect('mongodb://<user>:<pass>@apollo.modulusmongo.net:27017/a9xAtogi');
-// // mongoose.connect('mongodb://localhost/test');
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
@@ -29,77 +18,6 @@ app.use(express.static(__dirname + '/public'));                 // set the stati
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-// // define model =================
-// var Vote = mongoose.model('Vote', {
-//     gender: String,
-//     votes: { type: Number, default: 0 }
-// });
-
-
-// routes ======================================================================
-
-// api ---------------------------------------------------------------------
-// get votes
-// app.get('/api/votes', function(req, res) {
-
-//     // use mongoose to get all votes in the database
-//     Vote.find(function(err, votes) {
-
-//         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-//         if (err)
-//             res.send(err)
-
-//         res.json(votes); // return all votes in JSON format
-//     });
-// });
-
-//  app.get('/api/resetfemale', function(req, res) {
-
-//     // create a vote, information comes from AJAX request from Angular
-//     Vote.create({
-//         gender : 'female',
-//         votes : 0
-//     }, function(err, vote) {
-//         if (err)
-//             res.send(err);
-
-//         // get and return all the votes after you create another
-//         Vote.find(function(err, votes) {
-//             if (err)
-//                 res.send(err)
-//             res.json(votes);
-//         });
-//     });
-
-// });
-// app.get('/api/resetmale', function(req, res) {
-
-//   // create a vote, information comes from AJAX request from Angular
-//   Vote.create({
-//       gender : 'male',
-//       votes : 0
-//   }, function(err, vote) {
-//       if (err)
-//           res.send(err);
-
-//       // get and return all the votes after you create another
-//       Vote.find(function(err, votes) {
-//           if (err)
-//               res.send(err)
-//           res.json(votes);
-//       });
-//   });
-
-// });
-
-// app.get('/api/femalevote', function(req, res) {
-
-//     // create a vote, information comes from AJAX request from Angular
-//     Vote.update({gender: "female"}, { $inc: {female: 1} } ), function(err,doc){
-
-//     }
-
-// });
 
 app.get('/api/malevote', function(req, res) {
 
