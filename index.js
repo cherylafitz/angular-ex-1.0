3cda38b10365ff39865cbef0fefff275177ea716
cherylafitz <cherylafitzgerald@gmail.com>
2015-10-28 11:02:33 -0700

    test

--- index.js
+++ index.js
@@ -11,26 +11,7 @@ var methodOverride = require('method-override');
 // configuration =================
 
 // Connect to the db
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-// var MongoClient = mongodb.MongoClient;
-
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:27017/gender_votes';
-
-// Use connect method to connect to the Server
-mongoose.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
-});
+mongoose.connect('mongodb://<user>:<pass>@apollo.modulusmongo.net:27017/a9xAtogi');
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
